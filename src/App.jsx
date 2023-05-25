0c3c14e8b97e8c39fcce46cf2cc752e95c08f654
WOWmasterJulia <topazzz@ukr.net>
2023-05-25 14:35:51 +0300

    changes

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import user from './Profile/Profile.user';
+
+export const App = () => {
+   return (
+     <div>
+           <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+           />
+    </div>
+);
+};
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
